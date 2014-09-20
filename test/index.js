c3775bae6a3df122850a6c054a02400e6995280e
Thomas Leen <tl@thomasleen.com>
2014-09-20 11:19:10 -0400

    added grunt + test framework

---
+++ test/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* global describe, it, should */
+/* jshint expr: true */
+
+var pkg = require('../package.json');
+
+
+describe('test', function(){
+  it.skip('example test', function(){
+    (true).should.be.true;
+  });
+});
